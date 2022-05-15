510af40ba37a7b925cb8f74cefbf329a66e38b31
Carbon225 <carbon225@protonmail.com>
2022-05-15 08:33:00 +0200

    morph

--- src/CircleDetector/CircleDetector.cpp
+++ src/CircleDetector/CircleDetector.cpp
@@ -1,5 +1,7 @@
 #include "CircleDetector.h"
 
+#include <opencv2/imgproc.hpp>
+
 using namespace cv;
 
 #define BROWN_LO Scalar(28. / 360 * 255, 0.38 * 255, 0.7 * 255)
@@ -16,7 +18,19 @@ void CircleDetector::getColorMasks(InputArray src,
                                    OutputArray gold,
                                    OutputArray beige)
 {
-    inRange(src, BROWN_LO, BROWN_HI, brown);
-    inRange(src, GOLD_LO, GOLD_HI, gold);
-    inRange(src, BEIGE_LO, BEIGE_HI, beige);
+    Mat mask_brown, mask_gold, mask_beige;
+
+    inRange(src, BROWN_LO, BROWN_HI, mask_brown);
+    inRange(src, GOLD_LO, GOLD_HI, mask_gold);
+    inRange(src, BEIGE_LO, BEIGE_HI, mask_beige);
+
+    InputArray kernel = getStructuringElement(MORPH_ELLIPSE, Size(7, 7));
+
+    morphologyEx(mask_brown, mask_brown, MORPH_CLOSE, kernel, Point(-1, -1), 2);
+    morphologyEx(mask_gold, mask_gold, MORPH_CLOSE, kernel, Point(-1, -1), 2);
+    morphologyEx(mask_beige, mask_beige, MORPH_CLOSE, kernel, Point(-1, -1), 2);
+
+    morphologyEx(mask_brown, brown, MORPH_OPEN, kernel, Point(-1, -1), 2);
+    morphologyEx(mask_gold, gold, MORPH_OPEN, kernel, Point(-1, -1), 2);
+    morphologyEx(mask_beige, beige, MORPH_OPEN, kernel, Point(-1, -1), 2);
 }
