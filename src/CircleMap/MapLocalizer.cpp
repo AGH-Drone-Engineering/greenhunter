09ff98a2d501cec71175247464895c7023752aa4
Carbon225 <carbon225@protonmail.com>
2022-06-14 00:23:37 +0200

    azimuth correction

--- src/CircleMap/MapLocalizer.cpp
+++ src/CircleMap/MapLocalizer.cpp
@@ -4,22 +4,20 @@
 #include <boost/geometry.hpp>
 
 namespace bg = boost::geometry;
-typedef bg::model::point<double, 2, bg::cs::geographic<bg::radian>> GeopointRad;
-typedef bg::model::point<double, 2, bg::cs::geographic<bg::degree>> GeopointDeg;
 typedef bg::formula::vincenty_direct<double, true> vincenty;
 
 MapLocalizer::Coords MapLocalizer::localize(const cv::Point &frame_point,
                                             const Telemetry &telemetry,
                                             const CameraParams &camera_params)
 {
-    double ground_width = 2 * telemetry.altitude * tan(camera_params.fov_h * 0.5);
-    double ground_height = 2 * telemetry.altitude * tan(camera_params.fov_v * 0.5);
+    double ground_width = 2. * telemetry.altitude * tan(camera_params.fov_h * 0.5);
+    double ground_height = 2. * telemetry.altitude * tan(camera_params.fov_v * 0.5);
 
     double dx = (frame_point.x - 0.5 * camera_params.frame_width) * ground_width / camera_params.frame_width;
     double dy = (0.5 * camera_params.frame_height - frame_point.y) * ground_height / camera_params.frame_height;
 
     double dist = sqrt(dx*dx + dy*dy);
-    double azi_rad = -atan2(dy, dx) + M_PI_2;
+    double azi_rad = -atan2(dy, dx) + M_PI_2 + telemetry.azimuth;
 
     auto res = vincenty::apply(
         telemetry.position.get<0>(),
@@ -29,5 +27,5 @@ MapLocalizer::Coords MapLocalizer::localize(const cv::Point &frame_point,
         bg::srs::spheroid<double>()
     );
 
-    return Coords(res.lon2, res.lat2);
+    return {res.lon2, res.lat2};
 }
