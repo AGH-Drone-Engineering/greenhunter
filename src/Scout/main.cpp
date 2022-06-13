8ebf03781c44b78a48f6e36bcddccceadbc681e2
Carbon225 <carbon225@protonmail.com>
2022-06-14 00:43:44 +0200

    io executor

--- src/Scout/main.cpp
+++ src/Scout/main.cpp
@@ -1,18 +1,27 @@
+#include <iostream>
 #include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/thread.hpp>
 
 #include "Scout.h"
 
 int main(int argc, char **argv)
 {
     boost::asio::io_context io_context;
+    boost::asio::executor_work_guard<boost::asio::io_context::executor_type>
+        work = boost::asio::make_work_guard(io_context);
+    boost::thread io_thread(boost::bind(&boost::asio::io_context::run, &io_context));
 
     Scout scout(io_context, {});
 
-    std::thread io_thread([&] {
-        io_context.run();
-    });
-
-    scout.run();
+    try
+    {
+        scout.run();
+    }
+    catch (std::exception &ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+    }
 
     io_context.stop();
     io_thread.join();
